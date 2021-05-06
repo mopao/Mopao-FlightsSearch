004942785351415e4d3f9c58f3b776a697d22f11
franck cheuzem <franckcheuzem@MacBook-Pro-de-franck.local>
2021-05-06 03:39:30 -0400

    add title icon v2

--- views/flight_search.php
+++ views/flight_search.php
@@ -62,6 +62,7 @@ if (isset($_POST['search'])) {
    <title>
      MOPAO - flights search
    </title>
+   <link rel="shortcut icon" type="image/x-icon" href="imgs/plane.png" />
    <!-- library -->
    <link rel="stylesheet" href="css/jquery-ui.css" type="text/css">
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
--- views/flights_list.php
+++ views/flights_list.php
@@ -37,6 +37,7 @@ trim($_SESSION['flights_details']["adults"]), trim($_SESSION['flights_details'][
     <title>
       MOPAO - flights offers
     </title>
+    <link rel="shortcut icon" type="image/x-icon" href="imgs/plane.png" />
     <!-- library -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
     integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
